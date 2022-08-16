31f3c5eefa1840628d9e587dbdf6eb98283755c0
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2022-08-15 18:47:57 -0600

    Adding Lab01 instructions and solution

---
+++ 02. Laboratories/Laaboratory 01/Solution/iniciando-java-17-LesterAGarciaA97-main/src/main/java/gt/url/edu/App.java
@@ -0,0 +1,14 @@
+package gt.url.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var cadena = "mi version es 17";
+        System.out.println( cadena );
+    }
+}
